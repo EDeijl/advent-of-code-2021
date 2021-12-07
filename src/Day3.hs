67de19f7c0b3cded2c1e6be411afa03a10284b58
Erik Deijl <erikd@q42.nl>
2021-12-07 13:31:17 +0100

    Day 3 part 2

--- src/Day3.hs
+++ src/Day3.hs
@@ -11,11 +11,62 @@ answer = do
   let binEpsilons = epsilons transposedMatrix
   let decGamma = binToDec binGammas
   let decEpsilon = binToDec binEpsilons
+  let oxygenRating = binToDec $ getOxygenRating bitMatrix
+  let scrubberRating = binToDec $ getScrubberRating bitMatrix
+  let totalRating = oxygenRating * scrubberRating
   putStrLn $ "Gamma: " ++ show decGamma
   putStrLn $ "Epsilon: " ++ show decEpsilon
   putStrLn $ "Total: " ++ show (decGamma * decEpsilon)
+  putStrLn $ "Oxygen rating: " ++ show oxygenRating
+  putStrLn $ "Scrubber rating: " ++ show scrubberRating
+  putStrLn $ "Total rating: " ++ show totalRating
 
 
+getOxygenRating :: [[Int]] -> [Int]
+getOxygenRating matrix = head $ gammaReducer 0 matrix (head (transpose matrix))
+
+getScrubberRating :: [[Int]] -> [Int]
+getScrubberRating matrix = head $ epsilonReducer 0 matrix (head (transpose matrix))
+
+gammaReducer :: Int -> [[Int]] -> [Int] -> [[Int]]
+gammaReducer _ [] _ = []
+gammaReducer _ [x] _ = [x]
+gammaReducer i matrix gammaBits = gammaReducer (i+1) filtered (transpose filtered !! (i+1))
+  where
+    filtered = filterMatrixByGammaAtN matrix i (gamma gammaBits)
+
+epsilonReducer :: Int -> [[Int]] -> [Int] -> [[Int]]
+epsilonReducer _ [] _ = []
+epsilonReducer _ [x] _ = [x]
+epsilonReducer i matrix epsilonBits = epsilonReducer (i+1) filtered (transpose filtered !! (i+1))
+  where
+    filtered = filterMatrixByEpsilonAtN matrix i (epsilon epsilonBits)
+
+filterMatrixByEpsilonAtN :: [[Int]] -> Int -> Int -> [[Int]]
+filterMatrixByEpsilonAtN matrix n epsilon =  filter (\x -> x !! n == epsilon) matrix
+
+o2genr :: Int -> [[Int]] -> [[Int]]
+o2genr n matrix = filter (\x -> x !! n == gamma (transpose matrix !! n)) matrix
+
+-- run a mapping function zipped with an index
+mapWithIndex :: (Int -> [Int] -> [Int]) -> [[Int]] -> [[Int]]
+mapWithIndex f = zipWith f [0..]
+
+
+gammaAtN :: [[Int]] -> Int -> Int
+gammaAtN matrix n = gamma $ matrix !! n
+
+filterMatrixByGammaAtN :: [[Int]] -> Int -> Int -> [[Int]]
+filterMatrixByGammaAtN matrix n gamma = filter (\x -> x !! n == gamma) matrix
+
+hasGammaAtPositionN :: [Int] -> Int -> Int -> Bool
+hasGammaAtPositionN row n gamma = row !! n == gamma
+
+
+
+filterGamma :: [Int] -> [Int]
+filterGamma l@(x:xs) = if x == gamma l then l else []
+filterGamma _ = []
 
 -- collect gammas from list
 gammas :: [[Int]] -> [Int]
@@ -38,15 +89,17 @@ epsilon xs = head $ minimumBy (comparing length) $ group $ sort xs
 binToDec :: [Int] -> Int
 binToDec = foldl (\acc x -> acc * 2 + x) 0
 
+-- utilities
+
 readFileIntoListOfBits :: String -> IO [[Int]]
 readFileIntoListOfBits fileName = do
   fileContents <- readFile fileName
   return $ linesToListsOfListOfBits fileContents
 
-
+-- convert a string of lines to a list of lists of bits
 linesToListsOfListOfBits :: String -> [[Int]]
 linesToListsOfListOfBits input = map stringToListOfBits $ lines input
 
-
+-- convert a string to a list of bits
 stringToListOfBits :: String -> [Int]
 stringToListOfBits = map (\x -> if x == '1' then 1 else 0)
--- test/Spec.hs
+++ test/Spec.hs
@@ -2,10 +2,34 @@ import Test.HUnit
 import Data.List
 import System.Exit
 import Day3
+import Day3 (getOxygenRating, getScrubberRating)
 
 testString = "00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010"
-testMatrix = [ [0,0,1,0,0], [1,1,1,1,0], [1,0,1,1,0], [1,0,1,1,1], [1,0,1,0,1], [0,1,1,1,1], [0,0,1,1,1], [1,1,1,0,0], [1,0,0,0,0], [1,1,0,0,1], [0,0,0,1,0], [0,1,0,1,0] ]
+testMatrix = [ [0,0,1,0,0], 
+            [1,1,1,1,0], 
+            [1,0,1,1,0], 
+            [1,0,1,1,1], 
+            [1,0,1,0,1], 
+            [0,1,1,1,1], 
+            [0,0,1,1,1], 
+            [1,1,1,0,0], 
+            [1,0,0,0,0], 
+            [1,1,0,0,1], 
+            [0,0,0,1,0], 
+            [0,1,0,1,0] ]
 
+o2genr_index_0 = [ [1,1,1,1,0],
+            [1,0,1,1,0], 
+            [1,0,1,1,1], 
+            [1,0,1,0,1], 
+            [1,1,1,0,0], 
+            [1,0,0,0,0], 
+            [1,1,0,0,1] ]
+
+o2genr_index_1 = [ [1,0,1,1,0],
+            [1,0,1,1,1],
+            [1,0,1,0,1],
+            [1,0,0,0,0]]
 
 testGammaValues = [1,0,1,1,0]
 testEpsilonValue = [0,1,0,0,1]
@@ -15,13 +39,22 @@ testGammas = TestCase(assertEqual "1,0,1,1,0" testGammaValues (gammas $ transpos
 testEpsilons = TestCase(assertEqual "0,1,0,0,1" testEpsilonValue (epsilons $ transpose testMatrix))
 testBinToDec = TestCase(assertEqual "binary 22" 22 (binToDec testGammaValues))
 
+testGetOxygenRating = TestCase(assertEqual "oxygen rating" [1,0,1,1,1] (getOxygenRating testMatrix)) 
+testGetScrubberRating = TestCase(assertEqual "scrubber rating" [0,1,0,1,0] (getScrubberRating testMatrix)) 
+
+testo2genr_at_0 = TestCase(assertEqual "at position 1" o2genr_index_0 (o2genr 0 testMatrix))
+testo2genr_at_1 = TestCase(assertEqual "at position 2" o2genr_index_1 (o2genr 1 o2genr_index_0))
 main :: IO ()
 main = do
   counts <- runTestTT ( test [
     testStringToListOfBits,
     testGammas,
     testEpsilons,
-    testBinToDec
+    testBinToDec,
+    testo2genr_at_0,
+    testo2genr_at_1,
+    testGetOxygenRating,
+    testGetScrubberRating
     ])
   if errors counts + failures counts == 0
     then exitSuccess
